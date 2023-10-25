927b836a814cb35061f7fba13c4eaf7a50cd19f4
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-10-25 21:53:11 +0700

    add fitur: pagination

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -15,33 +15,51 @@ import axios from "axios";
 import NoResult from "@/components/Global/NoResult";
 
 const HomePage = ({ data }) => {
-	const { users, skills } = data;
 	const hasWindow = typeof window !== "undefined";
 
 	const [user_id, setUser_id] = useState(0);
+	const [users, setUsers] = useState([]);
+	const [skills, setSkills] = useState([]);
+	const [sort, setSort] = useState("ASC");
+	const [page, setPage] = useState(1);
+	const [search, setSearch] = useState("");
+	const limit = 5;
+
+	const { pagination } = users;
+
+	const getUsers = async (search, sort, page, limit) => {
+		const response = await axios.get(
+			`${baseUrl}/user/worker?search=${search}&sort=${sort}&page=${page}&limit=${limit}`,
+		);
+		setUsers(response.data);
+	};
+
+	const getSkills = async () => {
+		const responseSkill = await axios.get(`${baseUrl}/skill`);
+
+		setSkills(responseSkill.data.data);
+	};
 
 	useEffect(() => {
 		if (hasWindow) {
 			const peworld_user_id = localStorage.getItem("peworld_user_id");
 			setUser_id(peworld_user_id);
 		}
-	}, [hasWindow]);
 
-	const pagination = {
-		totalPage: 5,
-		currentPage: 3,
-	};
+		getUsers(search, sort, page, limit);
+		getSkills();
+	}, [hasWindow, sort, page, search]);
 
 	const handlePrevious = () => {
-		console.log("Halaman sebelumnya");
+		if (page > 1) {
+			setPage((prev) => prev - 1);
+		}
 	};
 
 	const handleNext = () => {
-		console.log("Halaman berikutnya");
-	};
-
-	const setPage = (pageNumber) => {
-		console.log(`Pindah ke halaman ${pageNumber}`);
+		if (pagination?.totalPage > page) {
+			setPage((prev) => prev + 1);
+		}
 	};
 
 	return (
@@ -53,7 +71,7 @@ const HomePage = ({ data }) => {
 					<h1 className="w-11/12 mx-auto px-6 text-2xl font-semibold">Top Jobs</h1>
 				</div>
 
-				<section className="pt-12 bg-[#F6F7F8]">
+				<section className="pt-12 bg-[#F6F7F8] min-h-[100vh]">
 					<div className="w-[87vw] px-10 rounded-sm justify-center  mx-auto grid h-13 py-1 items-center bg-white">
 						<div className="flex">
 							<div className="relative">
@@ -84,9 +102,9 @@ const HomePage = ({ data }) => {
 						</div>
 					</div>
 
-					<div className="divide-y mt-12 divide-gray-300 w-[88%] mx-auto rounded-md overflow-hidden">
-						{users?.length > 0 ? (
-							users
+					<div className="divide-y mt-12 divide-gray-300 w-[88%] mx-auto rounded-md overflow-hidden min-h-[100vh]">
+						{users?.data?.length > 0 ? (
+							users?.data
 								?.filter((item) => item?.user_id != user_id)
 								?.map((item, index) => (
 									<article
@@ -140,19 +158,19 @@ const HomePage = ({ data }) => {
 					<div className="w-full py-12 ">
 						<div className="flex justify-center items-center gap-2">
 							<button
-								className="text-sm h-9 w-9 items-center flex justify-center bg-white border border-gray-200 rounded-sm text-gray-400"
-								onClick={handleNext}>
+								className="text-sm h-9 w-9 items-center flex justify-center hover:bg-indigo-50 bg-white border border-gray-200 rounded-sm text-gray-400"
+								onClick={handlePrevious}>
 								<ChevronLeftIcon className="w-5 h-5" />
 							</button>
-							{Array.from({ length: 5 }).map((_, index) => {
+							{Array.from({ length: pagination?.totalPage }).map((_, index) => {
 								let pageNumber = index + 1;
 
 								return (
 									<button
 										className={`text-sm rounded-sm ${
-											pagination.currentPage === pageNumber
+											pagination?.currentPage === pageNumber
 												? "bg-[#5E50A1] text-white"
-												: "bg-white text-gray-400"
+												: "bg-white text-gray-400 hover:bg-indigo-50"
 										} border border-gray-200 h-9 w-9`}
 										onClick={() => setPage(pageNumber)}
 										key={index}>
@@ -162,7 +180,7 @@ const HomePage = ({ data }) => {
 							})}
 
 							<button
-								className="text-sm h-9 w-9 items-center flex justify-center bg-white border border-gray-200 rounded-sm text-gray-400"
+								className="text-sm h-9 w-9 items-center flex justify-center hover:bg-indigo-50 bg-white border border-gray-200 rounded-sm text-gray-400"
 								onClick={handleNext}>
 								<ChevronRightIcon className="w-5 h-5" />
 							</button>
@@ -177,18 +195,3 @@ const HomePage = ({ data }) => {
 };
 
 export default HomePage;
-
-export async function getStaticProps(req, res) {
-	const response = await axios.get(`${baseUrl}/user/worker`);
-	const responseSkill = await axios.get(`${baseUrl}/skill`);
-
-	return {
-		props: {
-			data: {
-				users: response.data.data,
-				skills: responseSkill.data.data,
-			},
-		},
-		revalidate: 10,
-	};
-}
